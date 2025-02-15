939bd1967398ffc91f6c926417bf4d7b0443bc92
Aamir saleem lone <120358827+hackmack4772@users.noreply.github.com>
2025-02-15 17:18:05 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+const firebaseConfig = const firebaseConfig = {
+  apiKey: "AIzaSyBtm9WpPn-WT0IFVKOW6Xs-dcX474oW16o",
+  authDomain: "hackmack4772.firebaseapp.com",
+  projectId: "hackmack4772",
+  storageBucket: "hackmack4772.firebasestorage.app",
+  messagingSenderId: "38931846023",
+  appId: "1:38931846023:web:2ed96aa0b1912c0fa6b922",
 };
 
 const app = initializeApp(firebaseConfig);
